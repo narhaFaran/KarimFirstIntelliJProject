0ecf3479e5d4ebe97069e607e439d09b5029f95f
yila-wen <jhuang4836@bths.edu>
2023-09-26 12:30:52 -0400

    mini project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Goodbye World!");
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat money = new DecimalFormat("0.00"); //help from Henk; format method for rounding
+        System.out.println("Enter bill:");
+        double bill = scanner.nextDouble();
+        System.out.println("Enter tip percentage:");
+        double tipPercent = scanner.nextDouble()/100;
+        System.out.println("Enter number of people:");
+        int numPeople = scanner.nextInt();
+
+        //calculations
+        double totalTip = bill*tipPercent;
+        double totalBill = bill + totalTip;
+        double tipPer = totalTip/numPeople;
+        double totalPer = totalBill/numPeople;
+
+        //bunch of print statements
+        System.out.println();
+        System.out.println("Number of people: " + numPeople);
+        System.out.println("Bill: $" + money.format(bill));
+        System.out.println("Tip Percent: %" + tipPercent*100);
+        System.out.println("Tip: $" + money.format(totalTip));
+        System.out.println("Total: $" + money.format(totalBill));
+        System.out.println("Tip Per Person: $" + money.format(tipPer));
+        System.out.println("Total Per Person: $" + money.format(totalPer));
+
     }
 }
