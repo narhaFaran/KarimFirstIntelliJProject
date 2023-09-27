3466e86ce98d38ca8a6dd1dbdfc65df2619d5af0
narhaF <farhank9@nycstudents.net>
2023-09-27 11:43:40 -0400

    reformatted printing

--- src/BillCalc.java
+++ src/BillCalc.java
@@ -1,5 +1,6 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class BillCalc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,14 +19,17 @@ public class BillCalc {
         double totalPer = totalBill/numPeople;
 
         //bunch of print statements
-        System.out.println();
-        System.out.println("Number of people: " + numPeople);
-        System.out.println("Bill: $" + money.format(bill));
-        System.out.println("Tip Percent: %" + tipPercent*100);
-        System.out.println("Tip: $" + money.format(totalTip));
-        System.out.println("Total: $" + money.format(totalBill));
-        System.out.println("Tip Per Person: $" + money.format(tipPer));
-        System.out.println("Total Per Person: $" + money.format(totalPer));
+        System.out.println("=================================================");
+        System.out.println("                    BILL");
+        System.out.println("=================================================");
+        System.out.println("Number of people:           " + numPeople);
+        System.out.println("Bill:                      $" + money.format(bill));
+        System.out.println("Tip Percent:               %" + tipPercent*100);
+        System.out.println("Tip:                       $" + money.format(totalTip));
+        System.out.println("Total:                     $" + money.format(totalBill));
+        System.out.println("Tip Per Person:            $" + money.format(tipPer));
+        System.out.println("Total Per Person:          $" + money.format(totalPer));
+        System.out.println("=================================================");
 
     }
 }
